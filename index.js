6a5e6841bf3fd8f3de70652b6a7e020127e9c6da
Maksim <kozemakomaksim2002@gmail.com>
2023-05-20 01:02:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById('menu');
+const bar = document.getElementById('bar');
+const main__dishes = document.getElementById('main__dishes');
+const main__bar = document.getElementById('main__bar');
+
+menu.addEventListener('click', ()=>{
+   main__bar.classList.add('__close');
+   main__dishes.classList.remove('__close');
+})
+bar.addEventListener('click', ()=>{
+   main__bar.classList.remove('__close');
+   main__dishes.classList.add('__close');
+})
